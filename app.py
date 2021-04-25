c21d9a6553c6721c04bbb170516fdfd8452ef737
MuzammilAarif <muzammilaarif12@gmail.com>
2021-04-25 22:49:39 +0530

    first

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import os
 
 
 app = Flask(__name__)
-PORT = int(os.environ.get('PORT', 4000))
+PORT = int(os.environ.get('PORT', 3000))
+
 
 @app.route("/", methods=["GET", "POST"])
 def startpy():
@@ -13,40 +14,11 @@ def startpy():
     return render_template("index.html")
 
 
-'''
-http://0.0.0.0:3000/api/data
-'''
-
-
 @app.route("/api/data", methods=["GET"])
 def api_get_data():
 
     result = fn.get_data()
     return jsonify(result)
-
-
-'''
-http://0.0.0.0:3000/api/add
-http://0.0.0.0:3000/api/add?food=cake&carbohydrates=20&protein=10&fat=5
-'''
-
-
-# @app.route("/api/add", methods=["GET"])
-# def api_add_data():
-#     food = request.values.get('food')
-#     carbohydrates = request.values.get('carbohydrates')
-#     protein = request.values.get('protein')
-#     fat = request.values.get('fat')
-
-#     result = {
-#         'food': food,
-#         'carbohydrates': carbohydrates,
-#         'protein': protein,
-#         'fat': fat
-
-#     }
-#     result_data = fn.add_row(food, carbohydrates, protein, fat)
-#     return jsonify(result)
 
 
 if __name__ == "__main__":
--- data.py
+++ data.py
@@ -1,41 +1,26 @@
-import pandas as pd 
+import pandas as pd
 
 
 def get_data():
 
     df = pd.read_csv('values.csv')
-    
+
     print(df['food'].tolist())
-    food            = df['food'].tolist()
-    carbohydrates   = df['carbohydrates'].tolist()
-    protein         = df['protein'].tolist()
-    fat             = df['fat'].tolist()
-   
+    food = df['food'].tolist()
+    carbohydrates = df['carbohydrates'].tolist()
+    protein = df['protein'].tolist()
+    fat = df['fat'].tolist()
+
     south_food = {
-        'food'           : food,
-        'carbohydrates'  : carbohydrates,
-        'protein'        : protein,
-        'fat'            : fat
-        
+        'food': food,
+        'carbohydrates': carbohydrates,
+        'protein': protein,
+        'fat': fat
+
     }
 
     print(south_food)
     return south_food
-
-
-# def add_row(food, carbohydrates,protein,fat ):
-#     df = pd.read_csv('values.csv') 
-#     new_food = {
-#         'food'           : food,
-#         'carbohydrates'  : carbohydrates,
-#         'protein'        : protein,
-#         'fat'            : fat
-        
-#     }
-#     print(df)
-#     df = df.append(new_row, ignore_index=True)
-#     print(df)
-#     df.to_csv('values.csv')
 
 
 if __name__ == "__main__":
